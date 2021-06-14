fca5aa6db872e05b18c0999a6413bd0f20a2f5e3
sameena <sameenah.tm@gmail.com>
2021-06-14 15:03:00 +0200

    final commit

---
+++ larger.js
@@ -0,0 +1,9 @@
+var r =require('readline-sync')
+num1=r.question('enter num1')
+num2=r.question('ener num2')
+if(num1>num2){
+    console.log('num1 is larger')
+}else{
+    console.log('num2 is larger')
+}
+
